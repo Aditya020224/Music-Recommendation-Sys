2f22d0dd530fb98bd7e36dea7e8de8bc5f393e3a
Aditya <96903392+Aditya020224@users.noreply.github.com>
2023-05-11 23:13:10 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as components
 
 @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_csv("data/filtered_track_df.csv")
+    df = pd.read_csv("data/processed_track_df.csv")
     df['genres'] = df.genres.apply(lambda x: [i[1:-1] for i in str(x)[1:-1].split(", ")])
     exploded_track_df = df.explode("genres")
     return exploded_track_df
