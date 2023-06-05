695294cde0de08a6b7de3ed2b226c04fca327d1b
Aditya <96903392+Aditya020224@users.noreply.github.com>
2023-06-05 13:29:01 +0530

    app.py

--- app.py
+++ app.py
@@ -37,66 +37,65 @@ def register_page():
         else:
             st.warning("Please enter all of the required details.")
 
-
-    @st.cache(allow_output_mutation=True)
-    def load_data():
-        df = pd.read_csv("data/processed_track_df.csv")
-        df['genres'] = df.genres.apply(lambda x: [i[1:-1] for i in str(x)[1:-1].split(", ")])
-        exploded_track_df = df.explode("genres")
-        return exploded_track_df
+@st.cache(allow_output_mutation=True)
+def load_data():
+    df = pd.read_csv("data/processed_track_df.csv")
+    df['genres'] = df.genres.apply(lambda x: [i[1:-1] for i in str(x)[1:-1].split(", ")])
+    exploded_track_df = df.explode("genres")
+    return exploded_track_df
     
-    genre_names = ['Dance Pop', 'Electronic', 'Electropop', 'Hip Hop', 'Jazz', 'K-pop', 'Latin', 'Pop', 'Pop Rap', 'R&B', 'Rock']
-    audio_feats = ["acousticness", "danceability", "energy", "instrumentalness", "valence", "tempo"]
-    exploded_track_df = load_data()
-    def n_neighbors_uri_audio(genre, start_year, end_year, test_feat):
-        genre = genre.lower()
-        genre_data = exploded_track_df[(exploded_track_df["genres"]==genre) & (exploded_track_df["release_year"]>=start_year) & (exploded_track_df["release_year"]<=end_year)]
-        genre_data = genre_data.sort_values(by='popularity', ascending=False)[:500]
-        neigh = NearestNeighbors()
-        neigh.fit(genre_data[audio_feats].to_numpy())
-        n_neighbors = neigh.kneighbors([test_feat], n_neighbors=len(genre_data), return_distance=False)[0]
+genre_names = ['Dance Pop', 'Electronic', 'Electropop', 'Hip Hop', 'Jazz', 'K-pop', 'Latin', 'Pop', 'Pop Rap', 'R&B', 'Rock']
+audio_feats = ["acousticness", "danceability", "energy", "instrumentalness", "valence", "tempo"]
+exploded_track_df = load_data()
+def n_neighbors_uri_audio(genre, start_year, end_year, test_feat):
+    genre = genre.lower()
+    genre_data = exploded_track_df[(exploded_track_df["genres"]==genre) & (exploded_track_df["release_year"]>=start_year) & (exploded_track_df["release_year"]<=end_year)]
+    genre_data = genre_data.sort_values(by='popularity', ascending=False)[:500]
+    neigh = NearestNeighbors()
+    neigh.fit(genre_data[audio_feats].to_numpy())
+    n_neighbors = neigh.kneighbors([test_feat], n_neighbors=len(genre_data), return_distance=False)[0]
         
-        uris = genre_data.iloc[n_neighbors]["uri"].tolist()
-        audios = genre_data.iloc[n_neighbors][audio_feats].to_numpy()
-        return uris, audios
-    def page():
-        title = "Music Recommendation System"
-        st.title(title)
+    uris = genre_data.iloc[n_neighbors]["uri"].tolist()
+    audios = genre_data.iloc[n_neighbors][audio_feats].to_numpy()
+    return uris, audios
+def page():
+    title = "Music Recommendation System"
+    st.title(title)
         
-        st.write("Welcome! Here you can customize what you want to listen to based on genres. And listen to the songs recommended by our system!")
-        st.markdown("##")
+    st.write("Welcome! Here you can customize what you want to listen to based on genres. And listen to the songs recommended by our system!")
+    st.markdown("##")
         
-        with st.container():
-            col1, col2,col3,col4 = st.columns((2,0.5,0.5,0.5))
-            with col3:
-                st.sidebar.header("***Select genre:***")
-                genre = st.sidebar.radio(
-                    "",
-                    genre_names, index=genre_names.index("K-Pop"))
-                with col1:
-                    st.markdown("***Customize Features :***")
-                    start_year, end_year = st.slider(
-                        'Select the year range',
-                        1990, 2019, (2010, 2019)
-                    )
-                    acousticness = st.slider(
-                        'Acousticness',
-                        0.0, 1.0, 0.5)
-                    danceability = st.slider(
-                        'Danceability',
-                        0.0, 1.0, 0.5)
-                    energy = st.slider(
-                        'Energy',
-                        0.0, 1.0, 0.5)
-                    instrumentalness = st.slider(
-                        'Instrumentalness',
-                        0.0, 1.0, 0.0)
-                    valence = st.slider(
-                        'Valence',
-                        0.0, 1.0, 0.45)
-                    tempo = st.slider(
-                        'Tempo',
-                        0.0, 244.0, 118.0)
+    with st.container():
+        col1, col2,col3,col4 = st.columns((2,0.5,0.5,0.5))
+        with col3:
+            st.sidebar.header("***Select genre:***")
+            genre = st.sidebar.radio(
+                "",
+                genre_names, index=genre_names.index("K-Pop"))
+            with col1:
+                st.markdown("***Customize Features :***")
+                start_year, end_year = st.slider(
+                    'Select the year range',
+                    1990, 2019, (2010, 2019)
+                )
+                acousticness = st.slider(
+                    'Acousticness',
+                    0.0, 1.0, 0.5)
+                danceability = st.slider(
+                    'Danceability',
+                    0.0, 1.0, 0.5)
+                energy = st.slider(
+                    'Energy',
+                    0.0, 1.0, 0.5)
+                instrumentalness = st.slider(
+                    'Instrumentalness',
+                    0.0, 1.0, 0.0)
+                valence = st.slider(
+                    'Valence',
+                    0.0, 1.0, 0.45)
+                tempo = st.slider(
+                    'Tempo',
+                    0.0, 244.0, 118.0)
                     
     tracks_per_page = 6
     test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]
