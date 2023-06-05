1846f26c8030fb876815944f7cfaf541e6c6d8ab
Aditya <96903392+Aditya020224@users.noreply.github.com>
2023-06-05 12:51:23 +0530

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
 import streamlit as st
-st.set_page_config(page_title="Song Recommendation", layout="wide")
-
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import plotly.express as px
 import streamlit.components.v1 as components
+st.set_page_config(page_title="Music Recommendation", layout="wide")
 
+# Create a login form
+def login_page():
+    st.title("User Login")
+    username = st.text_input("Username", key="username")
+    password1 = st.text_input("Password", type="password", key="password1")
+
+# If the user enters a valid username and password, log them in
+    if st.button("Login"):
+        if username == "admin" and password1 == "password":
+             st.success("Login successful!")
+             st.session_state["is_logged_in"] = True
+             st.experimental_rerun()  # Rerun the app to show the recommendation page
+        else:
+             st.warning("Invalid username or password.")
+
+# Create a registration form
+def register_page():
+    st.write("Enter your details below to create a new account.")
+    first_name = st.text_input("First name", key="first_name")
+    last_name = st.text_input("Last name", key="last_name")
+    email = st.text_input("Email", key="email")
+    password2 = st.text_input("Password", type="password", key="password2")
+
+# If the user enters all of the required details, create the account
+    if st.button("Create Account"):
+        if first_name and last_name and email and password2:
+            st.success("Account created successfully!")
+            st.session_state["is_logged_in"] = True
+            st.experimental_rerun()  # Rerun the app to show the recommendation page
+        else:
+            st.warning("Please enter all of the required details.")
+
+
+def page():
 @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_csv("data/filtered_track_df.csv")
+    df = pd.read_csv("data/processed_track_df.csv")
     df['genres'] = df.genres.apply(lambda x: [i[1:-1] for i in str(x)[1:-1].split(", ")])
     exploded_track_df = df.explode("genres")
     return exploded_track_df
@@ -33,24 +66,24 @@ def n_neighbors_uri_audio(genre, start_year, end_year, test_feat):
     return uris, audios
 
 def page():
-    title = "Song Recommendation Engine"
+    title = "Music Recommendation System"
     st.title(title)
 
-    st.write("First of all, welcome! This is the place where you can customize what you want to listen to based on genre and several key audio features. Try playing around with different settings and listen to the songs recommended by our system!")
+    st.write("Welcome! Here you can customize what you want to listen to based on genres. And listen to the songs recommended by our system!")
     st.markdown("##")
 
     with st.container():
         col1, col2,col3,col4 = st.columns((2,0.5,0.5,0.5))
         with col3:
-            st.markdown("***Choose your genre:***")
-            genre = st.radio(
+            st.sidebar.header("***Select genre:***")
+            genre = st.sidebar.radio(
                 "",
-                genre_names, index=genre_names.index("Pop"))
+                genre_names, index=genre_names.index("K-Pop"))
         with col1:
-            st.markdown("***Choose features to customize:***")
+            st.markdown("***Customize Features :***")
             start_year, end_year = st.slider(
                 'Select the year range',
-                1990, 2019, (2015, 2019)
+                1990, 2019, (2010, 2019)
             )
             acousticness = st.slider(
                 'Acousticness',
